a03a8165cf301440cbb7ecd19c6d72d0d9051443
maxfox101 <maks280706@gmail.com>
2025-06-15 01:57:17 +0300

    Create json.h

---
+++ json.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <vector>
+#include <stdexcept>
+#include <initializer_list>
+
+namespace json {
+
+    class Node;
+    using Dict = std::map<std::string, Node>;
+    using Array = std::vector<Node>;
+
+    class ParsingError : public std::runtime_error {
+    public:
+        using runtime_error::runtime_error;
+    };
+
+    class Node {
+    public:
+        using Value = std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>;
+
+        Node() = default;
+        explicit Node(std::nullptr_t);
+        explicit Node(Array array);
+        explicit Node(Dict map);
+        explicit Node(bool value);
+        explicit Node(int value);
+        explicit Node(double value);
+        explicit Node(const char* value);
+        explicit Node(std::string value);
+
+        // Добавлены конструкторы для списков инициализации
+        Node(std::initializer_list<Node> list);
+        Node(std::initializer_list<std::pair<const std::string, Node>> list);
+
+        bool IsNull() const;
+        bool IsArray() const;
+        bool IsMap() const;
+        bool IsBool() const;
+        bool IsInt() const;
+        bool IsDouble() const;
+        bool IsPureDouble() const;
+        bool IsString() const;
+
+        const Array& AsArray() const;
+        const Dict& AsMap() const;
+        bool AsBool() const;
+        int AsInt() const;
+        double AsDouble() const;
+        const std::string& AsString() const;
+
+        const Value& GetValue() const { return value_; }
+
+        bool operator==(const Node& rhs) const;
+        bool operator!=(const Node& rhs) const;
+        bool operator==(const Array& arr) const;
+        bool operator==(const Dict& dict) const;
+
+    private:
+        Value value_;
+    };
+
+    class Document {
+    public:
+        explicit Document(Node root);
+        explicit Document(Array array);
+        explicit Document(Dict dict);
+
+        const Node& GetRoot() const;
+
+    private:
+        Node root_;
+    };
+
+    Document Load(std::istream& input);
+    void Print(const Document& doc, std::ostream& output);
+
+}  // namespace json
